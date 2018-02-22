e2dbd9d05966d8ff80456b32ce8ac36e120e53be
zand <zabahey.sukoi@gmail.com>
2018-02-22 22:22:52 +0700

    prevnt submit from form

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -13,9 +13,13 @@ export default class SearchBar extends Component {
         this.setState({term: event.target.value});
     }
 
+    onFormSubmit(event) {
+        event.preventDefault();
+    }
+
     render() {
         return (
-            <form className="input-group">
+            <form onSubmit={this.onFormSubmit} className="input-group">
                 <input
                   placeholder="Get a five-day forcast in your favorite cities"
                   className="form-control"
