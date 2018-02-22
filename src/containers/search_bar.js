e0101072af85c80a51cfd668ba3d99e860d797f6
zand <zabahey.sukoi@gmail.com>
2018-02-22 16:47:05 +0700

    implement search bar and bind context to event handler on input change

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { term: '' };
+
+        this.onInputChange = this.onInputChange.bind(this);
+    }
+
+    onInputChange(event) {
+        this.setState({term: event.target.value});
+    }
+
     render() {
         return (
             <form className="input-group">
-                <input />
+                <input
+                  placeholder="Get a five-day forcast in your favorite cities"
+                  className="form-control"
+                  value={this.state.term}
+                  onChange={this.onInputChange}     
+                />
                 <span className="input-group-btn">
                     <button className="btn btn-secondary" type="submit">Submit</button>
                 </span>
