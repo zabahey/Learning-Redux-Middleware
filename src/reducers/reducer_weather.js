1c13f29785b0f166b2ccaecf5ff265e9ed96c52f
zand <zabahey.sukoi@gmail.com>
2018-02-23 22:42:31 +0700

    implement reducer to response for only FETCH_WEATHER action

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,3 +1,11 @@
-export default function(state=null, action) {
+import { FETCH_WEATHER } from "../actions/index";
+// Must not mutate state in reducer. It must be return completely new state instead
+export default function(state= [], action) {
+
+    switch (action.type) {
+        case FETCH_WEATHER:
+            //return state.concat([action.payload.data]);
+            return [ action.payload.data, ...state];// [city, city, city]
+    }
     return state;
 }
