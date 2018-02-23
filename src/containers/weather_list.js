33b08c50a3694cad0552e521f24342601811328c
zand <zabahey.sukoi@gmail.com>
2018-02-23 23:49:00 +0700

    display chart for temperatures with sparkline package

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,13 +1,22 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { Sparklines, SparklinesLine } from "react-sparklines";
 
 class WeatherList extends Component {
     renderWeather(cityData) {
         const name = cityData.city.name;
-
+        const  temperatures = cityData.list.map(weather => weather.main.temp);
+        const  pressures = cityData.list.map(weather => weather.main.pressure);
+        const  preshumiditysures = cityData.list.map(weather => weather.main.humidity);
+        
         return (
             <tr key={name}>
                 <td>{name}</td>
+                <td>
+                    <Sparklines height={120} width={180} data={temperatures}>
+                        <SparklinesLine color="red" />
+                    </Sparklines>
+                </td>
             </tr>
         );
     }
