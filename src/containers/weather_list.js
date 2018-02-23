e693f67c5db9b922cd3b3deceb587acceb73e737
zand <zabahey.sukoi@gmail.com>
2018-02-24 00:23:24 +0700

    Display units of chart

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -7,13 +7,19 @@ class WeatherList extends Component {
         const name = cityData.city.name;
         const  temperatures = cityData.list.map(weather => weather.main.temp);
         const  pressures = cityData.list.map(weather => weather.main.pressure);
-        const  preshumiditysures = cityData.list.map(weather => weather.main.humidity);
+        const  humidities = cityData.list.map(weather => weather.main.humidity);
         
         return (
             <tr key={name}>
                 <td>{name}</td>
                 <td>
-                    <Chart data={temperatures} color="orange" />
+                    <Chart data={temperatures} color="orange" units="K" />
+                </td>
+                <td>
+                    <Chart data={pressures} color="green" units="hPa"/>
+                </td>
+                <td>
+                    <Chart data={humidities} color="black" units="%"/>
                 </td>
             </tr>
         );
@@ -24,9 +30,9 @@ class WeatherList extends Component {
                 <thead>
                     <tr>
                         <th>City</th>
-                        <th>Temperature</th>
-                        <th>Pressure</th>
-                        <th>Humidity</th>
+                        <th>Temperature (K)</th>
+                        <th>Pressure (hPa)</th>
+                        <th>Humidity (%)</th>
                     </tr>
                 </thead>
                 <tbody>
