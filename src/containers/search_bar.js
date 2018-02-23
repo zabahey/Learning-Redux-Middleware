196aa28e1600e8d78403797ddfd5d4c77d257cc3
zand <zabahey.sukoi@gmail.com>
2018-02-24 00:04:23 +0700

    create chart component for reuse

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -27,17 +27,17 @@ class SearchBar extends Component {
 
     render() {
         return (
-            <form onSubmit={this.onFormSubmit} className="input-group">
-                <input
-                  placeholder="Get a five-day forcast in your favorite cities"
-                  className="form-control"
-                  value={this.state.term}
-                  onChange={this.onInputChange}     
-                />
-                <span className="input-group-btn">
-                    <button className="btn btn-secondary" type="submit">Submit</button>
-                </span>
-            </form>
+                <form onSubmit={this.onFormSubmit} className="input-group">
+                    <input
+                    placeholder="Get a five-day forcast in your favorite cities"
+                    className="form-control"
+                    value={this.state.term}
+                    onChange={this.onInputChange}     
+                    />
+                    <span className="input-group-btn">
+                        <button className="btn btn-secondary" type="submit">Submit</button>
+                    </span>
+                </form>
         );
     }
 }
