2809ff80cb5ab917568de2adb00486652f227dc8
zand <zabahey.sukoi@gmail.com>
2018-02-27 21:58:50 +0700

    add reset weather action

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,6 +4,7 @@ const API_KEY = '';
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
+export const RESET_WEATHER = 'RESET_WEATHER';
 
 export function fectchWeather(city) {
     const url =`${ROOT_URL}&q=${city},us`;
@@ -12,5 +13,12 @@ export function fectchWeather(city) {
     return {
         type: FETCH_WEATHER,
         payload: request
+    };
+}
+
+export function resetWeather() {
+    return {
+        type: RESET_WEATHER,
+        payload: []
     };
 }
